9d1e23fe1c0e218fe9167ed7ec1aaf7478174304
Ochyy <ochydiza@gmail.com>
2026-02-02 15:32:16 +0700

    fix use databse

--- bot.js
+++ bot.js
@@ -28,13 +28,7 @@ app.listen(PORT, () => {
 });
 
 /* ================= DATABASE ================= */
-const db = await mysql.createPool({
-  host: process.env.DB_HOST,
-  port: process.env.DB_PORT || 3306,
-  user: process.env.DB_USER,
-  password: process.env.DB_PASSWORD,
-  database: process.env.DB_NAME,
-});
+const db = await mysql.createPool(process.env.DATABASE_URL);
 
 /* ================= SESSION ================= */
 const sessions = {};
